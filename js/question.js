de25d9026116fa9b65851300bc11da2b6d46d760
DrDisagree <crazymahmud08@gmail.com>
2023-05-26 23:22:51 +0600

    first commit

---
+++ js/question.js
@@ -0,0 +1,111 @@
+let questions = [{
+  numb: 1,
+  question: 'Jeff Bezos is the founder of?',
+  answer: 'Amazon',
+  options: [
+    "Subway",
+    "Tesla",
+    "Amazon",
+    "Apple"
+  ]
+},
+{
+  numb: 2,
+  question: "Lemurs are native to which country?",
+  answer: "Madagascar",
+  options: [
+    "Peru",
+    "Madagascar",
+    "Cuba",
+    "India"
+  ]
+},
+{
+  numb: 3,
+  question: "First colour television come out in?",
+  answer: "1928",
+  options: [
+    "1936",
+    "1954",
+    "1945",
+    "1928"
+  ]
+},
+{
+  numb: 4,
+  question: "The Australian Flag has how many colours?",
+  answer: "3",
+  options: [
+    "2",
+    "3",
+    "5",
+    "4"
+  ]
+},
+{
+  numb: 5,
+  question: "What was Walt Disney afraid of?",
+  answer: "Mice",
+  options: [
+    "Spiders",
+    "Mice",
+    "Dogs",
+    "Cats"
+  ]
+},
+{
+  numb: 6,
+  question: "Name of the smallest bone in human body?",
+  answer: "Stapes",
+  options: [
+    "Mandible",
+    "Maxilla",
+    "Ulna",
+    "Stapes"
+  ]
+},
+{
+  numb: 7,
+  question: "Which of the following is NOT a fruit?",
+  answer: "Rhubarb",
+  options: [
+    "Rhubarb",
+    "Tomatoes",
+    "Avocados",
+    "Walnut"
+  ]
+},
+{
+  numb: 8,
+  question: "Where was the first example of paper money used?",
+  answer: "China",
+  options: [
+    "Greece",
+    "Turkey",
+    "China",
+    "England"
+  ]
+},
+{
+  numb: 9,
+  question: "The fear of insects is known as what?",
+  answer: "Entomophobia",
+  options: [
+    "Arachnophobia",
+    "Entomophobia",
+    "Ailurophobia",
+    "Mysophobia"
+  ]
+},
+{
+  numb: 10,
+  question: "How long did dinosaurs live on the earth?",
+  answer: "150-200 million years",
+  options: [
+    "100-150 million years",
+    "50-100 million years",
+    "200+ million years",
+    "150-200 million years"
+  ]
+},
+]
